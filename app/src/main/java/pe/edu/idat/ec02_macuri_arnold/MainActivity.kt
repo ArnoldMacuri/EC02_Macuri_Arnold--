17e96dd4b8df535d211b2cee7ddb7ae6167903d3
ArnoldMacuri <arnold.macuri@gmail.com>
2024-07-01 21:22:02 -0500

    Iniciando Ejercicio

--- app/src/main/java/pe/edu/idat/ec02_macuri_arnold/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/ec02_macuri_arnold/MainActivity.kt
@@ -6,9 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pe.edu.idat.ec02_macuri_arnold.ui.theme.EC02_Macuri_ArnoldTheme
@@ -19,29 +25,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EC02_Macuri_ArnoldTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Ejercicio1(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+        Formulario()
+    }
+}
+
+@Composable
+fun Formulario(){
+    var nombre by remember {
+        mutableStateOf("")
+    }
+    var apellido by remember {
+        mutableStateOf("")
+    }
+    var dni by remember {
+        mutableStateOf("")
+    }
+    var celular by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+    var seleccionarPrograma by remember {
+        mutableStateOf(listOf<String>())
+    }
+    var seleccionarColor by remember {
+        mutableStateOf("")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    EC02_Macuri_ArnoldTheme {
-        Greeting("Android")
-    }
+
+
 }
